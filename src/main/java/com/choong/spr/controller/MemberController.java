6ae4bffc45b6eeaf117685dbf042d2989fe15333
sebaek <sebaek@gmail.com>
2022-05-25 09:42:03 +0900

    add member process

--- src/main/java/com/choong/spr/controller/MemberController.java
+++ src/main/java/com/choong/spr/controller/MemberController.java
@@ -1,15 +1,48 @@
 package com.choong.spr.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.choong.spr.domain.MemberDto;
 
 @Controller
 @RequestMapping("member")
 public class MemberController {
+	
+	@Autowired
+	private MemberService service;
+	
+	// TODO : MemberService 작성
+	//                      addMember method 작성
+	//        MemberMapper.java, xml 작성
+	//                      insertMember method 작성
+	
 
 	@GetMapping("signup")
 	public void signupForm() {
 		
 	}
+	
+	@PostMapping("signup")
+	public void signupProcess(MemberDto member) {
+		boolean success = service.addMember(member);
+		
+		if (success) {
+			
+		} else {
+			
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
