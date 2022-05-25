8098b040bf5362341afec82dee8fce9e0ce5132c
sebaek <sebaek@gmail.com>
2022-05-25 14:44:30 +0900

    controller

--- src/main/java/com/choong/spr/controller/MemberController.java
+++ src/main/java/com/choong/spr/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.choong.spr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -82,6 +85,15 @@ public class MemberController {
 			return "ok";
 		}
 	}
+	
+	@GetMapping("list")
+	// jsp (id, password, email, nickName, inserted) table로 보여주세요.
+	// ORDER BY inserted DESC
+	public void list(Model model) {
+		List<MemberDto> list = service.listMember();
+		model.addAttribute("memberList", list);
+	}
+	
 }
 
 
